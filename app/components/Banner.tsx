5102cb994902f3c7038b75a3add7569e9b97092c
JulianeAdamy <86632678+JulianeAdamy@users.noreply.github.com>
2023-10-22 17:39:25 +0000

    fix: ban

--- app/components/Banner.tsx
+++ app/components/Banner.tsx
@@ -1,16 +1,17 @@
 export default  function Banner() {
     return (
         <div>
-            <div className="md:flex items-center flex-row-reverse">
-                <div className="basis-1/2 text-center text-black">
-                <h2 className="text-lg">E-Commerce Powerhouse</h2>
-  <h3 className="text-lg">Made by UTFPR</h3>
-                    <img src="https://github.com/JulianeAdamy/siteutfpr/assets/86632678/05f7a463-c8ee-47d2-afd0-967abe4bbe50" alt="Descrição da Imagem" />
-                   
-                </div>
-
-                
-            </div>
+            <div className="flex items-center basis-1/2 text-black">
+  <div className="mr-4">
+    <h2>E-Commerce Powerhouse</h2>
+    <h3>Made by UTFPR</h3>
+  </div>
+  <img 
+    src="https://github.com/JulianeAdamy/siteutfpr/assets/86632678/febf4fdb-0b2e-4ffb-a2d9-6145e015aee5" 
+    alt="Descrição da Imagem"
+    style={{ width: '100%', maxWidth: '200px', height: 'auto' }} 
+  />
+</div>
 
         </div>
     )
