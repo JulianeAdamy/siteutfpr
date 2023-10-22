117c6197cbb2313ec35df2220cc8b398e668c7bc
JulianeAdamy <86632678+JulianeAdamy@users.noreply.github.com>
2023-10-22 17:53:41 +0000

    fix: banner

--- app/components/Banner.tsx
+++ app/components/Banner.tsx
@@ -1,16 +1,16 @@
 export default  function Banner() {
     return (
         <div>
-            <div className="flex items-center justify-center h-screen text-center text-black">
+           <div className="flex items-center justify-center h-screen text-center text-black">
   <div className="flex items-center">
-    <div className="mr-4">
-      <h2 className="text-2xl">E-Commerce Powerhouse</h2>
-      <h3 className="text-2xl">Made by UTFPR</h3>
+    <div className="mr-4" style={{ fontSize: '50px' }}>
+      <h2>E-Commerce Powerhouse</h2>
+      <h3>Made by UTFPR</h3>
     </div>
     <img 
       src="https://github.com/JulianeAdamy/siteutfpr/assets/86632678/febf4fdb-0b2e-4ffb-a2d9-6145e015aee5" 
       alt="Descrição da Imagem"
-      style={{ width: '100%', maxWidth: '200px', height: 'auto' }} 
+      style={{ width: '50%', maxWidth: '100px', height: 'auto' }} 
     />
   </div>
 </div>
