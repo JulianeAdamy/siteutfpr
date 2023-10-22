fd92b6f4ad4a140cab0ea3b03189591bb06d3fd2
JulianeAdamy <86632678+JulianeAdamy@users.noreply.github.com>
2023-10-22 17:49:22 +0000

    fix: imag

--- app/components/Banner.tsx
+++ app/components/Banner.tsx
@@ -2,17 +2,18 @@ export default  function Banner() {
     return (
         <div>
             <div className="flex items-center justify-center h-screen text-center text-black">
-  <div className="flex flex-col items-center">
-    <h2>E-Commerce Powerhouse</h2>
-    <h3>Made by UTFPR</h3>
+  <div className="flex items-center">
+    <div className="mr-4">
+      <h2 className="text-2xl">E-Commerce Powerhouse</h2>
+      <h3 className="text-2xl">Made by UTFPR</h3>
+    </div>
     <img 
       src="https://github.com/JulianeAdamy/siteutfpr/assets/86632678/febf4fdb-0b2e-4ffb-a2d9-6145e015aee5" 
       alt="Descrição da Imagem"
-      style={{ width: '50%', maxWidth: '100px', height: 'auto' }} 
+      style={{ width: '100%', maxWidth: '200px', height: 'auto' }} 
     />
   </div>
 </div>
-
         </div>
     )
 }
