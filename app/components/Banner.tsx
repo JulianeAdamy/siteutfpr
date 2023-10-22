96803a165e1fdece5b79805f85769e056c26ecff
JulianeAdamy <86632678+JulianeAdamy@users.noreply.github.com>
2023-10-22 13:28:00 +0000

    fix: imagem

--- app/components/Banner.tsx
+++ app/components/Banner.tsx
@@ -3,7 +3,7 @@ export default  function Banner() {
         <div>
             <div className="md:flex items-center flex-row-reverse">
                 <div className="basis-1/2">
-                    <img src="/img/banner.svg" alt="Descrição da Imagem" />
+                    <img src="![foca](https://github.com/JulianeAdamy/siteutfpr/assets/86632678/04b1bff3-941a-46f4-9ff3-74bb3a0a8e81)" alt="Descrição da Imagem" />
                 </div>
                 <div className="basis-1/2 text-center text-black">
                     <h2>E-Commerce Powerhouse</h2>
