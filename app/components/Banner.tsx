d826d308340487c6733018510ba4dfc025a46c7a
JulianeAdamy <86632678+JulianeAdamy@users.noreply.github.com>
2023-10-22 17:23:36 +0000

    fix: imagem

--- app/components/Banner.tsx
+++ app/components/Banner.tsx
@@ -3,13 +3,12 @@ export default  function Banner() {
         <div>
             <div className="md:flex items-center flex-row-reverse">
                 <div className="basis-1/2">
-                    <img src="https://github.com/JulianeAdamy/siteutfpr/assets/86632678/1220d0ea-69cd-4e6a-bea8-b468bc9cdc06" alt="Descrição da Imagem" />
+                    <img src="https://github.com/JulianeAdamy/siteutfpr/assets/86632678/05f7a463-c8ee-47d2-afd0-967abe4bbe50" alt="Descrição da Imagem" />
                    
                 </div>
-                <div className="basis-1/2 text-center text-black">
+                <div className="basis-1/2 text-center text-black text-lg">
                     <h2>E-Commerce Powerhouse</h2>
                     <h3>Made by UTFPR</h3>
-                   
                 </div>
             </div>
 
