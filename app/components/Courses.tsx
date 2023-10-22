365e1274383f68572ee13eec0dbe759f05c35fcf
JulianeAdamy <86632678+JulianeAdamy@users.noreply.github.com>
2023-10-22 18:29:19 +0000

    fix: imagem

--- app/components/Courses.tsx
+++ app/components/Courses.tsx
@@ -17,7 +17,7 @@ export default function Courses() {
             </div>
             <div className="bg-[#000000] rounded-md pb-2">
                 <a href="/curso_css.html">
-                    <img src="/img/css.svg" alt="" />
+                    <img src="https://github.com/JulianeAdamy/siteutfpr/assets/86632678/62b56b62-bcd2-4512-9d4c-e649f0bc9138" alt="" />
                     <div className="text-white text-center">
                         <h3>Curso de CSS</h3>
                         <p>O curso é realizado por profissionais..</p>
@@ -26,7 +26,7 @@ export default function Courses() {
             </div>
             <div className="bg-[#000000] rounded-md pb-2">
                 <a href="/curso_js.html">
-                    <img src="/img/js.svg" alt="" />
+                    <img src="https://github.com/JulianeAdamy/siteutfpr/assets/86632678/24fdb27c-365e-4235-99ca-d5cc20f5ec4c" alt="" />
                     <div className="text-white text-center">
                         <h3>Curso de JS</h3>
                         <p>O curso é realizado por profissionais..</p>
@@ -35,7 +35,7 @@ export default function Courses() {
             </div>
             <div className="bg-[#000000] rounded-md pb-2">
                 <a href="/curso_html.html">
-                    <img src="/img/design.svg" alt="" />
+                    <img src="https://github.com/JulianeAdamy/siteutfpr/assets/86632678/ace9daec-e5af-4d7e-9fe8-2034a48283b7" alt="" />
                     <div className="text-white text-center">
                         <h3>Curso de DESIGN</h3>
                         <p>O curso é realizado por profissionais..</p>
@@ -44,7 +44,7 @@ export default function Courses() {
             </div>
             <div className="bg-[#000000] rounded-md pb-2">
                 <a href="/curso_html.html">
-                    <img src="/img/games.svg" alt="" />
+                    <img src="https://github.com/JulianeAdamy/siteutfpr/assets/86632678/5e54835a-152c-4067-b4c6-9c16a1b238d5" alt="" />
                     <div className="text-white text-center">
                         <h3>Curso de GAMES</h3>
                         <p>O curso é realizado por profissionais..</p>
@@ -53,7 +53,7 @@ export default function Courses() {
             </div>
             <div className="bg-[#000000] rounded-md pb-2">
                 <a href="/curso_html.html">
-                    <img src="/img/robot.svg" alt="" />
+                    <img src="https://github.com/JulianeAdamy/siteutfpr/assets/86632678/05feca88-5283-415e-aac6-5ba20b209c76" alt="" />
                     <div className="text-white text-center">
                         <h3>Curso de ROBÓTICA</h3>
                         <p>O curso é realizado por profissionais..</p>
