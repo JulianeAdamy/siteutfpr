aa9a590bef40d24e2a5f49d2472325fd11e4513b
JulianeAdamy <86632678+JulianeAdamy@users.noreply.github.com>
2023-10-22 17:29:35 +0000

    fix: tex

--- app/components/Banner.tsx
+++ app/components/Banner.tsx
@@ -6,9 +6,9 @@ export default  function Banner() {
                     <img src="https://github.com/JulianeAdamy/siteutfpr/assets/86632678/05f7a463-c8ee-47d2-afd0-967abe4bbe50" alt="Descrição da Imagem" />
                    
                 </div>
-                <div className="basis-1/2 text-center text-black text-lg">
-                    <h2>E-Commerce Powerhouse</h2>
-                    <h3>Made by UTFPR</h3>
+                <div className="basis-1/2 text-center text-black">
+                <h2 className="text-lg">E-Commerce Powerhouse</h2>
+  <h3 className="text-lg">Made by UTFPR</h3>
                 </div>
             </div>
 
