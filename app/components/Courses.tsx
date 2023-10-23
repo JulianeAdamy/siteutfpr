ee600761daed510b00c4ebf34e327a926e30be25
JulianeAdamy <86632678+JulianeAdamy@users.noreply.github.com>
2023-10-23 18:01:50 +0000

    fix: atualizacao

--- app/components/Courses.tsx
+++ app/components/Courses.tsx
@@ -17,130 +17,158 @@ export default function Courses() {
     </div>
   </a>
 </div>
-            <div className="bg-[#000000] rounded-md pb-2">
+            <div className="bg-[#FFFFFF] rounded-md pb-2 border border-gray-300">
                 <a href="/curso_css.html">
                     <img src="https://github.com/JulianeAdamy/siteutfpr/assets/86632678/62b56b62-bcd2-4512-9d4c-e649f0bc9138" alt="" />
-                    <div className="text-white text-center">
-                        <h3>Curso de CSS</h3>
-                        <p>O curso é realizado por profissionais..</p>
+                    <div className="text-black text-left space-x-4 space-y-4">
+    
+    <h3>       </h3>
+    <p className="text-black-500">ipad pro 05 standar... $ 22</p>
+      <p className="text-gray-500">f11bc5b1 Photoshop</p>
                     </div>
                 </a>
             </div>
-            <div className="bg-[#000000] rounded-md pb-2">
+            <div className="bg-[#FFFFFF] rounded-md pb-2 border border-gray-300">
                 <a href="/curso_js.html">
                     <img src="https://github.com/JulianeAdamy/siteutfpr/assets/86632678/0bed05e5-2a3d-4835-9f28-d83f02896d6f" alt="" />
-                    <div className="text-white text-center">
-                        <h3>Curso de JS</h3>
-                        <p>O curso é realizado por profissionais..</p>
+                    <div className="text-black text-left space-x-4 space-y-4">
+    
+    <h3>       </h3>
+    <p className="text-black-500">ipad pro 05 standar... $ 22</p>
+      <p className="text-gray-500">f11bc5b1 Photoshop</p>
                     </div>
                 </a>
             </div>
-            <div className="bg-[#000000] rounded-md pb-2">
+            <div className="bg-[#FFFFFF] rounded-md pb-2 border border-gray-300">
                 <a href="/curso_html.html">
                     <img src="https://github.com/JulianeAdamy/siteutfpr/assets/86632678/72607c62-a0b0-403b-b60a-a7949a5d52b9" alt="" />
-                    <div className="text-white text-center">
-                        <h3>Curso de DESIGN</h3>
-                        <p>O curso é realizado por profissionais..</p>
+                    <div className="text-black text-left space-x-4 space-y-4">
+    
+    <h3>       </h3>
+    <p className="text-black-500">ipad pro 05 standar... $ 22</p>
+      <p className="text-gray-500">f11bc5b1 Photoshop</p>
                     </div>
                 </a>
             </div>
-            <div className="bg-[#000000] rounded-md pb-2">
+            <div className="bg-[#FFFFFF] rounded-md pb-2 border border-gray-300">
                 <a href="/curso_html.html">
                     <img src="https://github.com/JulianeAdamy/siteutfpr/assets/86632678/da82fdc4-580b-421f-a332-46af76b976c6" alt="" />
-                    <div className="text-white text-center">
-                        <h3>Curso de GAMES</h3>
-                        <p>O curso é realizado por profissionais..</p>
+                    <div className="text-black text-left space-x-4 space-y-4">
+    
+    <h3>       </h3>
+    <p className="text-black-500">ipad pro 05 standar... $ 22</p>
+      <p className="text-gray-500">f11bc5b1 Photoshop</p>
                     </div>
                 </a>
             </div>
-            <div className="bg-[#000000] rounded-md pb-2">
+            <div className="bg-[#FFFFFF] rounded-md pb-2 border border-gray-300">
                 <a href="/curso_html.html">
                     <img src="https://github.com/JulianeAdamy/siteutfpr/assets/86632678/05feca88-5283-415e-aac6-5ba20b209c76" alt="" />
-                    <div className="text-white text-center">
-                        <h3>Curso de ROBÓTICA</h3>
-                        <p>O curso é realizado por profissionais..</p>
+                    <div className="text-black text-left space-x-4 space-y-4">
+    
+    <h3>       </h3>
+    <p className="text-black-500">ipad pro 05 standar... $ 22</p>
+      <p className="text-gray-500">f11bc5b1 Photoshop</p>
                     </div>
                 </a>
             </div>
 
-            <div className="bg-[#000000] rounded-md pb-2">
+            <div className="bg-[#FFFFFF] rounded-md pb-2 border border-gray-300">
                 <a href="/curso_html.html">
                     <img className="hover:scale-105" src="https://github.com/JulianeAdamy/siteutfpr/assets/86632678/3be4b90a-0437-4370-9a98-36f20362b08e" alt="" />
-                    <div className="text-white text-center">
-                        <h3>Curso de HTML</h3>
-                        <p>O curso é realizado por profissionais..</p>
+                    <div className="text-black text-left space-x-4 space-y-4">
+    
+    <h3>       </h3>
+    <p className="text-black-500">ipad pro 05 standar... $ 22</p>
+      <p className="text-gray-500">f11bc5b1 Photoshop</p>
                     </div>
                 </a>
             </div>
-            <div className="bg-[#000000] rounded-md pb-2">
+            <div className="bg-[#FFFFFF] rounded-md pb-2 border border-gray-300">
                 <a href="/curso_css.html">
                     <img src="https://github.com/JulianeAdamy/siteutfpr/assets/86632678/62b56b62-bcd2-4512-9d4c-e649f0bc9138" alt="" />
-                    <div className="text-white text-center">
-                        <h3>Curso de CSS</h3>
-                        <p>O curso é realizado por profissionais..</p>
+                    <div className="text-black text-left space-x-4 space-y-4">
+    
+    <h3>       </h3>
+    <p className="text-black-500">ipad pro 05 standar... $ 22</p>
+      <p className="text-gray-500">f11bc5b1 Photoshop</p>
                     </div>
                 </a>
             </div>
-            <div className="bg-[#000000] rounded-md pb-2">
+            <div className="bg-[#FFFFFF] rounded-md pb-2 border border-gray-300">
                 <a href="/curso_js.html">
                     <img src="https://github.com/JulianeAdamy/siteutfpr/assets/86632678/0bed05e5-2a3d-4835-9f28-d83f02896d6f" alt="" />
-                    <div className="text-white text-center">
-                        <h3>Curso de JS</h3>
-                        <p>O curso é realizado por profissionais..</p>
+                    <div className="text-black text-left space-x-4 space-y-4">
+    
+    <h3>       </h3>
+    <p className="text-black-500">ipad pro 05 standar... $ 22</p>
+      <p className="text-gray-500">f11bc5b1 Photoshop</p>
                     </div>
                 </a>
             </div>
-            <div className="bg-[#000000] rounded-md pb-2">
+            <div className="bg-[#FFFFFF] rounded-md pb-2 border border-gray-300">
                 <a href="/curso_html.html">
                     <img src="https://github.com/JulianeAdamy/siteutfpr/assets/86632678/72607c62-a0b0-403b-b60a-a7949a5d52b9" alt="" />
-                    <div className="text-white text-center">
-                        <h3>Curso de DESIGN</h3>
-                        <p>O curso é realizado por profissionais..</p>
+                    <div className="text-black text-left space-x-4 space-y-4">
+    
+    <h3>       </h3>
+    <p className="text-black-500">ipad pro 05 standar... $ 22</p>
+      <p className="text-gray-500">f11bc5b1 Photoshop</p>
                     </div>
                 </a>
             </div>
-            <div className="bg-[#000000] rounded-md pb-2">
+            <div className="bg-[#FFFFFF] rounded-md pb-2 border border-gray-300">
                 <a href="/curso_html.html">
                     <img src="https://github.com/JulianeAdamy/siteutfpr/assets/86632678/da82fdc4-580b-421f-a332-46af76b976c6" alt="" />
-                    <div className="text-white text-center">
-                        <h3>Curso de GAMES</h3>
-                        <p>O curso é realizado por profissionais..</p>
+                    <div className="text-black text-left space-x-4 space-y-4">
+    
+    <h3>       </h3>
+    <p className="text-black-500">ipad pro 05 standar... $ 22</p>
+      <p className="text-gray-500">f11bc5b1 Photoshop</p>
                     </div>
                 </a>
             </div>
-            <div className="bg-[#000000] rounded-md pb-2">
+            <div className="bg-[#FFFFFF] rounded-md pb-2 border border-gray-300">
                 <a href="/curso_html.html">
                     <img src="https://github.com/JulianeAdamy/siteutfpr/assets/86632678/05feca88-5283-415e-aac6-5ba20b209c76" alt="" />
-                    <div className="text-white text-center">
-                        <h3>Curso de ROBÓTICA</h3>
-                        <p>O curso é realizado por profissionais..</p>
+                    <div className="text-black text-left space-x-4 space-y-4">
+    
+    <h3>       </h3>
+    <p className="text-black-500">ipad pro 05 standar... $ 22</p>
+      <p className="text-gray-500">f11bc5b1 Photoshop</p>
                     </div>
                 </a>
                 </div>
-                <div className="bg-[#000000] rounded-md pb-2">
+                <div className="bg-[#FFFFFF] rounded-md pb-2 border border-gray-300">
                 <a href="/curso_html.html">
                     <img src="https://github.com/JulianeAdamy/siteutfpr/assets/86632678/72607c62-a0b0-403b-b60a-a7949a5d52b9" alt="" />
-                    <div className="text-white text-center">
-                        <h3>Curso de DESIGN</h3>
-                        <p>O curso é realizado por profissionais..</p>
+                    <div className="text-black text-left space-x-4 space-y-4">
+    
+    <h3>       </h3>
+    <p className="text-black-500">ipad pro 05 standar... $ 22</p>
+      <p className="text-gray-500">f11bc5b1 Photoshop</p>
                     </div>
                 </a>
             </div>
-            <div className="bg-[#000000] rounded-md pb-2">
+            <div className="bg-[#FFFFFF] rounded-md pb-2 border border-gray-300">
                 <a href="/curso_html.html">
                     <img src="https://github.com/JulianeAdamy/siteutfpr/assets/86632678/da82fdc4-580b-421f-a332-46af76b976c6" alt="" />
-                    <div className="text-white text-center">
-                        <h3>Curso de GAMES</h3>
-                        <p>O curso é realizado por profissionais..</p>
+                    <div className="text-black text-left space-x-4 space-y-4">
+    
+    <h3>       </h3>
+    <p className="text-black-500">ipad pro 05 standar... $ 22</p>
+      <p className="text-gray-500">f11bc5b1 Photoshop</p>
                     </div>
                 </a>
             </div>
-            <div className="bg-[#000000] rounded-md pb-2">
+            <div className="bg-[#FFFFFF] rounded-md pb-2 border border-gray-300">
                 <a href="/curso_html.html">
                     <img src="https://github.com/JulianeAdamy/siteutfpr/assets/86632678/05feca88-5283-415e-aac6-5ba20b209c76" alt="" />
-                    <div className="text-white text-center">
-                        <h3>Curso de ROBÓTICA</h3>
-                        <p>O curso é realizado por profissionais..</p>
+                    <div className="text-black text-left space-x-4 space-y-4">
+    
+    <h3>       </h3>
+    <p className="text-black-500">ipad pro 05 standar... $ 22</p>
+      <p className="text-gray-500">f11bc5b1 Photoshop</p>
                     </div>
                 </a>
                 </div>
