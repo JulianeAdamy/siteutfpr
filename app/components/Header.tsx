199c9150b448b15b61c065aae0d82b13bbbf9582
JulianeAdamy <86632678+JulianeAdamy@users.noreply.github.com>
2023-10-23 23:39:05 +0000

    fix: tama

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,6 +1,6 @@
 export default function Header() {
     return (
-        <header className="text-center text-black py-8 px-20 border-b border-[#000000] md:flex itens-center justify-between">
+        <header className="text-center text-black text-2xl py-8 px-20 border-b border-[#000000] md:flex itens-center justify-between">
             <h4>UTFPR</h4>
             <nav>
                 <a 
@@ -9,13 +9,13 @@ export default function Header() {
                 >
                     Discover
                 </a>
-                <a className="space-x-4 hover:text-[#C0C0C0]" href="#cursos">
+                <a className="space-x-12 hover:text-[#C0C0C0]" href="#cursos">
                     Templates &nbsp;
                 </a>
-                <a className="space-x-4 hover:text-[#C0C0C0]" href="#contato">
+                <a className="space-x-12 hover:text-[#C0C0C0]" href="#contato">
                     Mockups &nbsp;{" "}
                 </a>
-                <a className="space-x-4 hover:text-[#C0C0C0]" href="#">
+                <a className="space-x-12 hover:text-[#C0C0C0]" href="#">
                     Graphics &nbsp;
                 </a>
             </nav>
