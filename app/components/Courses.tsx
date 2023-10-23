75949d5801bd22986d1cde049f9918d15ec7a48e
JulianeAdamy <86632678+JulianeAdamy@users.noreply.github.com>
2023-10-23 19:01:01 +0000

    fix: tex

--- app/components/Courses.tsx
+++ app/components/Courses.tsx
@@ -1,7 +1,7 @@
 export default function Courses() {
     return (
         <main className="grid gap-12 md:grid-cols-2 lg:grid-cols-3">
-            <div className="md:col-span-2 lg:col-span-3 mt-4 text-black text-center">
+            <div className="md:col-span-2 lg:col-span-3 mt-4 text-black text-left">
                 <h2 id="cursos">
                     Latest <span>Drops</span>
                 </h2>
@@ -23,8 +23,8 @@ export default function Courses() {
                     <div className="text-black text-left space-x-4 space-y-4">
     
     <h3>       </h3>
-    <p className="text-black-500">ipad pro 05 standar... $ 22</p>
-      <p className="text-gray-500">f11bc5b1 Photoshop</p>
+    <p className="text-black-500">DahsFolio... $ 75</p>
+      <p className="text-gray-500">2bef6a36 Framer</p>
                     </div>
                 </a>
             </div>
@@ -34,10 +34,15 @@ export default function Courses() {
                     <div className="text-black text-left space-x-4 space-y-4">
     
     <h3>       </h3>
-    <p className="text-black-500">ipad pro 05 standar... $ 22</p>
-      <p className="text-gray-500">f11bc5b1 Photoshop</p>
+    <p className="text-black-500">Distortion Collecti... $ 19</p>
+      <p className="text-gray-500">c4b537ab Tallwind</p>
                     </div>
                 </a>
+            </div>
+            <div className="md:col-span-2 lg:col-span-3 mt-4 text-black text-center">
+                <h2 id="cursos">
+                    Latest <span>Drops</span>
+                </h2>
             </div>
             <div className="bg-[#FFFFFF] rounded-md pb-2 border border-gray-300">
                 <a href="/curso_html.html">
@@ -45,8 +50,8 @@ export default function Courses() {
                     <div className="text-black text-left space-x-4 space-y-4">
     
     <h3>       </h3>
-    <p className="text-black-500">ipad pro 05 standar... $ 22</p>
-      <p className="text-gray-500">f11bc5b1 Photoshop</p>
+    <p className="text-black-500">CyberFolio... $ 35</p>
+      <p className="text-gray-500">aee4b3c5 Framer</p>
                     </div>
                 </a>
             </div>
@@ -56,8 +61,8 @@ export default function Courses() {
                     <div className="text-black text-left space-x-4 space-y-4">
     
     <h3>       </h3>
-    <p className="text-black-500">ipad pro 05 standar... $ 22</p>
-      <p className="text-gray-500">f11bc5b1 Photoshop</p>
+    <p className="text-black-500">Future Folio... $ 49</p>
+      <p className="text-gray-500">76ce1a25 Framer</p>
                     </div>
                 </a>
             </div>
@@ -67,20 +72,24 @@ export default function Courses() {
                     <div className="text-black text-left space-x-4 space-y-4">
     
     <h3>       </h3>
-    <p className="text-black-500">ipad pro 05 standar... $ 22</p>
-      <p className="text-gray-500">f11bc5b1 Photoshop</p>
+    <p className="text-black-500">Anomaly... $ Free</p>
+      <p className="text-gray-500">25162b26 Framer</p>
                     </div>
                 </a>
             </div>
-
+            <div className="md:col-span-2 lg:col-span-3 mt-4 text-black text-center">
+                <h2 id="cursos">
+                    Latest <span>Drops</span>
+                </h2>
+            </div>
             <div className="bg-[#FFFFFF] rounded-md pb-2 border border-gray-300">
                 <a href="/curso_html.html">
                     <img className="hover:scale-105 rounded-lg" src="https://github.com/JulianeAdamy/siteutfpr/assets/86632678/3be4b90a-0437-4370-9a98-36f20362b08e" alt="" />
                     <div className="text-black text-left space-x-4 space-y-4">
     
     <h3>       </h3>
-    <p className="text-black-500">ipad pro 05 standar... $ 22</p>
-      <p className="text-gray-500">f11bc5b1 Photoshop</p>
+    <p className="text-black-500">MacBook Pro 08 Stan... $ 30</p>
+      <p className="text-gray-500">3edf655a lllustrator</p>
                     </div>
                 </a>
             </div>
@@ -90,8 +99,8 @@ export default function Courses() {
                     <div className="text-black text-left space-x-4 space-y-4">
     
     <h3>       </h3>
-    <p className="text-black-500">ipad pro 05 standar... $ 22</p>
-      <p className="text-gray-500">f11bc5b1 Photoshop</p>
+    <p className="text-black-500">Iphone 14 Pro Stand... $ 19</p>
+      <p className="text-gray-500">db787245 Photoshop</p>
                     </div>
                 </a>
             </div>
@@ -101,10 +110,15 @@ export default function Courses() {
                     <div className="text-black text-left space-x-4 space-y-4">
     
     <h3>       </h3>
-    <p className="text-black-500">ipad pro 05 standar... $ 22</p>
-      <p className="text-gray-500">f11bc5b1 Photoshop</p>
+    <p className="text-black-500">Wall Sign 01 Standa... $ 19</p>
+      <p className="text-gray-500">0b2ea682 Framer</p>
                     </div>
                 </a>
+            </div>
+            <div className="md:col-span-2 lg:col-span-3 mt-4 text-black text-center">
+                <h2 id="cursos">
+                    Latest <span>Drops</span>
+                </h2>
             </div>
             <div className="bg-[#FFFFFF] rounded-md pb-2 border border-gray-300">
                 <a href="/curso_html.html">
@@ -112,8 +126,8 @@ export default function Courses() {
                     <div className="text-black text-left space-x-4 space-y-4">
     
     <h3>       </h3>
-    <p className="text-black-500">ipad pro 05 standar... $ 22</p>
-      <p className="text-gray-500">f11bc5b1 Photoshop</p>
+    <p className="text-black-500">Distortion Collecti... $ 19</p>
+      <p className="text-gray-500">aee4b3c5 lllustrator</p>
                     </div>
                 </a>
             </div>
@@ -123,8 +137,8 @@ export default function Courses() {
                     <div className="text-black text-left space-x-4 space-y-4">
     
     <h3>       </h3>
-    <p className="text-black-500">ipad pro 05 standar... $ 22</p>
-      <p className="text-gray-500">f11bc5b1 Photoshop</p>
+    <p className="text-black-500">Transform Collection... $ 19</p>
+      <p className="text-gray-500">76ce1a25 Photoshop</p>
                     </div>
                 </a>
             </div>
@@ -134,19 +148,24 @@ export default function Courses() {
                     <div className="text-black text-left space-x-4 space-y-4">
     
     <h3>       </h3>
-    <p className="text-black-500">ipad pro 05 standar... $ 22</p>
-      <p className="text-gray-500">f11bc5b1 Photoshop</p>
+    <p className="text-black-500">Undertone 03 Gradie... $ 11</p>
+      <p className="text-gray-500">25162b26 Framer</p>
                     </div>
                 </a>
                 </div>
+                <div className="md:col-span-2 lg:col-span-3 mt-4 text-black text-center">
+                <h2 id="cursos">
+                    Latest <span>Drops</span>
+                </h2>
+            </div>
                 <div className="bg-[#FFFFFF] rounded-md pb-2 border border-gray-300">
                 <a href="/curso_html.html">
                     <img className="hover:scale-105 rounded-lg" src="https://github.com/JulianeAdamy/siteutfpr/assets/86632678/72607c62-a0b0-403b-b60a-a7949a5d52b9" alt="" />
                     <div className="text-black text-left space-x-4 space-y-4">
     
     <h3>       </h3>
-    <p className="text-black-500">ipad pro 05 standar... $ 22</p>
-      <p className="text-gray-500">f11bc5b1 Photoshop</p>
+    <p className="text-black-500">Distortion Collecti... $ 19</p>
+      <p className="text-gray-500">aee4b3c5 lllustrator</p>
                     </div>
                 </a>
             </div>
@@ -156,8 +175,8 @@ export default function Courses() {
                     <div className="text-black text-left space-x-4 space-y-4">
     
     <h3>       </h3>
-    <p className="text-black-500">ipad pro 05 standar... $ 22</p>
-      <p className="text-gray-500">f11bc5b1 Photoshop</p>
+    <p className="text-black-500">Transform Collection... $ 19</p>
+      <p className="text-gray-500">76ce1a25 Photoshop</p>
                     </div>
                 </a>
             </div>
@@ -167,8 +186,8 @@ export default function Courses() {
                     <div className="text-black text-left space-x-4 space-y-4">
     
     <h3>       </h3>
-    <p className="text-black-500">ipad pro 05 standar... $ 22</p>
-      <p className="text-gray-500">f11bc5b1 Photoshop</p>
+    <p className="text-black-500">Undertone 03 Gradie... $ 11</p>
+      <p className="text-gray-500">25162b26 Framer</p>
                     </div>
                 </a>
                 </div>
