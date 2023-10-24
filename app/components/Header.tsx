ee07fc9c1a9f4176d18062ab8818115820a15913
JulianeAdamy <86632678+JulianeAdamy@users.noreply.github.com>
2023-10-24 00:07:35 +0000

    fix: texto

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,7 +1,9 @@
 export default function Header() {
     return (
-        <header className="text-center text-black py-8 border-b border-[#000000] w-full md:flex itens-center justify-between">
-            <h4>UTFPR</h4>
+        <header 
+        className="text-center text-black py-8 border-b border-[#000000] md:flex itens-center justify-between">
+            
+            <h4>&nbsp;{" "}&nbsp;{" "}&nbsp;{" "}&nbsp;{" "}UTFPR</h4>
             <nav>
                 <a 
                     className="text-[#C0C0C0] bg-black px-2 py-[4px] rounded hover:bg-[#000000] hover:text-white"
