0080839fbc423b490f6f5576d94a9e165a20d4fd
JulianeAdamy <86632678+JulianeAdamy@users.noreply.github.com>
2023-10-24 00:24:37 +0000

    fix: tamanho

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -2,8 +2,13 @@ export default function Header() {
     return (
         <header 
         className="text-center text-black py-8 border-b border-[#000000] md:flex itens-center justify-between">
+            <div className="flex items-center">
+    <div className="mr-4" style={{ fontSize: '25px' }}>
+    <h4>&nbsp;{" "}&nbsp;{" "}&nbsp;{" "}&nbsp;{" "}&nbsp;{" "}&nbsp;{" "}&nbsp;{" "}&nbsp;{" "}UTFPR</h4>
+      
+    </div>
+    </div>
             
-            <h4>&nbsp;{" "}&nbsp;{" "}&nbsp;{" "}&nbsp;{" "}UTFPR</h4>
             <nav>
                 <a 
                     className="text-[#C0C0C0] bg-black px-2 py-[4px] rounded hover:bg-[#000000] hover:text-white"
