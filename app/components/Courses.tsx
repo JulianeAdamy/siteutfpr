f1f2d7dae17376d72c6ce76f49e5e8f51cb92c8d
JulianeAdamy <86632678+JulianeAdamy@users.noreply.github.com>
2023-10-26 19:15:52 +0000

    fix: borda

--- app/components/Courses.tsx
+++ app/components/Courses.tsx
@@ -197,10 +197,12 @@ export default function Courses() {
                     </div>
                 </a>
                 </div>
+                 <h3>   </h3>
                 <div className="text-center py-8 border-t border-[#808080] text-[#C0C0C0] md:flex items-center justify-between">
-                
+                <p className="text-gray-500">76ce1a25 Photoshop</p>
                 </div>
         </main>
+        
 
     )
 }
