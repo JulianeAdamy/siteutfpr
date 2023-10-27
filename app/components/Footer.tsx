c1bccc1c5ac83977034434160c294029ecebd359
JulianeAdamy <86632678+JulianeAdamy@users.noreply.github.com>
2023-10-27 20:32:15 +0000

    fix: ccor

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -13,7 +13,7 @@ export default function Footer () {
                 <div className="flex justify-end text-4xl text-center space-x-4">
                 <a href="">
                     <svg
-                        fill="gray"
+                        fill="silver"
                         xmlns="http://www.w3.org/2000/svg"
                         height="1em"
                         viewBox="0 0 512 512"
@@ -24,7 +24,7 @@ export default function Footer () {
                 </a>
                 <a href="">
                     <svg
-                        fill="gray"
+                        fill="silver"
                         xmlns="http://www.w3.org/2000/svg"
                         height="1em"
                         viewBox="0 0 448 512"
@@ -35,7 +35,7 @@ export default function Footer () {
                 </a>
                <a href="httt://www.facebook.com/seuenderece">
                     <svg
-                        fill="gray"
+                        fill="silver"
                         xmlns="http://www.w3.org/2000/svg"
                         height="1em"
                         viewBox="0 0 512 512"
