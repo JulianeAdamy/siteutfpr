139c1ce2f1c3edde52cab671c7b5f0c58b4730ab
JulianeAdamy <86632678+JulianeAdamy@users.noreply.github.com>
2023-10-27 00:36:31 +0000

    fix: final

---
+++ app/components/Final.tsx
@@ -0,0 +1,67 @@
+import React from 'react';
+
+const Column: React.FC<{ children: React.ReactNode }> = ({ children }) => {
+  return (
+    <div
+      style={{
+        width: '68px',
+        height: '24px',
+        fontFamily: 'Inter',
+        fontWeight: 400,
+        fontSize: '16px',
+        lineHeight: '24px'
+      }}
+    >
+      {children}
+    </div>
+  );
+};
+
+export default function Final() {
+  return (
+    <div style={{ display: 'flex', flexDirection: 'column' }}>
+      {/* Parte 1 */}
+      <div
+        style={{
+          width: '65px',
+          height: '28px',
+          fontFamily: 'Inter',
+          fontWeight: 400,
+          fontSize: '20px',
+          lineHeight: '28px'
+        }}
+      >
+        UTFPR
+      </div>
+
+      {/* Parte 2 */}
+      <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+        <Column>Products</Column>
+        <Column>Templates</Column>
+        <Column>Mockups</Column>
+        <Column>Graphics</Column>
+      </div>
+
+      {/* Parte 3 */}
+      <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+        <Column>About</Column>
+        <Column>Magazine</Column>
+        <Column>About Us</Column>
+      </div>
+
+      {/* Parte 4 */}
+      <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+        <Column>Help</Column>
+        <Column>Support</Column>
+        <Column>Contact</Column>
+      </div>
+
+      {/* Parte 5 */}
+      <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+        <Column>Legal</Column>
+        <Column>Privacy Policy</Column>
+        <Column>Terms & Conditions</Column>
+      </div>
+    </div>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import Banner  from "./components/Banner";
 import Courses from "./components/Courses";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
+import Final from "./components/Final";
 
 
 export default function Home() {
@@ -10,7 +11,9 @@ export default function Home() {
       <Header />
       <Banner />
       <Courses />
+      <Final/>
       <Footer />
+      
 
     </>
 
